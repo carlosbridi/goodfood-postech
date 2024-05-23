726c5e9bd9a7ed5b4c65fd34c782b08e3ebd181c
Carlos Bridi <carlosbridi@gmail.com>
2024-05-22 22:53:40 -0300

    minor fixes

--- src/main/java/com/good/food/adapter/outbound/ClienteDatabaseGatewayImpl.java
+++ src/main/java/com/good/food/adapter/outbound/ClienteDatabaseGatewayImpl.java
@@ -1,25 +1,28 @@
 package com.good.food.adapter.outbound;
 
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import com.good.food.adapter.outbound.repository.ClienteRepository;
+import com.good.food.adapter.outbound.repository.entity.ClienteEntity;
 import com.good.food.core.domain.Cliente;
 import com.good.food.core.ports.outbound.ClienteDatabaseGateway;
 
 @Component
 public class ClienteDatabaseGatewayImpl implements ClienteDatabaseGateway {
 
-//  @Autowired
-//  private ClienteRepository clienteRepository;
+  @Autowired
+  private ClienteRepository clienteRepository;
 
   @Override
   public Cliente save(Cliente cliente) {
-    return null; //clienteRepository.save(new ClienteEntity(cliente)).toDomain();
+    return clienteRepository.save(new ClienteEntity(cliente)).toDomain();
   }
 
   @Override
   public Cliente findByCpf(String cpf) {
-    return null;
-//    final Optional<ClienteEntity> findByCpf = clienteRepository.findByCpf(cpf);
-//    return findByCpf.isPresent() ? findByCpf.get().toDomain() : null;
+    final Optional<ClienteEntity> findByCpf = clienteRepository.findByCpf(cpf);
+    return findByCpf.isPresent() ? findByCpf.get().toDomain() : null;
   }
 
 }
