b7c19273c93865500c5ca0c9be18d904fbc6950d
Carlos Bridi <carlosbridi@gmail.com>
2024-05-23 09:55:17 -0300

    fix: ajuste busca de cpf

--- src/main/java/com/good/food/adapter/inbound/controller/ClienteController.java
+++ src/main/java/com/good/food/adapter/inbound/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package com.good.food.adapter.inbound.controller;
 
 import java.net.URI;
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.good.food.adapter.inbound.controller.request.ClienteRequest;
 import com.good.food.adapter.inbound.controller.response.ClienteResponse;
+import com.good.food.adapter.outbound.repository.ClienteRepository;
 import com.good.food.core.ports.inbound.BuscarClienteUseCase;
 import com.good.food.core.ports.inbound.CadastrarClienteUseCase;
+import com.good.food.domain.exceptions.NotFoundException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -48,7 +51,6 @@ public class ClienteController {
   @ApiResponses(
       value = {
           @ApiResponse(code = 200, message = "Ok"),
-          @ApiResponse(code = 400, message = "Bad request"),
       }
   )
   @ResponseStatus(code = HttpStatus.OK)
@@ -56,8 +58,8 @@ public class ClienteController {
   @Operation(summary = "Identificação do Cliente via CPF", description = "Procura o cliente pelo CPF")
   public ResponseEntity<ClienteResponse> findByCpf(@RequestParam String cpf){
     return 
-        ResponseEntity.ok().body(
-            new ClienteResponse(buscarCliente.execute(cpf)));
+        ResponseEntity.ok().body(Optional.ofNullable(buscarCliente.execute(cpf)).map(ClienteResponse::new)
+            .orElseThrow(() -> new NotFoundException("Cliente não encontrado com o CPF informado")));
   }
 
 }
--- src/main/java/com/good/food/core/usecase/BuscarCliente.java
+++ src/main/java/com/good/food/core/usecase/BuscarCliente.java
@@ -1,12 +1,10 @@
 package com.good.food.core.usecase;
 
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.good.food.core.domain.Cliente;
 import com.good.food.core.ports.inbound.BuscarClienteUseCase;
 import com.good.food.core.ports.outbound.ClienteDatabaseGateway;
-import com.good.food.domain.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -17,8 +15,7 @@ public class BuscarCliente implements BuscarClienteUseCase {
   private final ClienteDatabaseGateway clienteDatabaseGateway;
 
   public Cliente execute(final String cpf) {
-    return Optional.ofNullable(clienteDatabaseGateway.findByCpf(cpf))
-        .orElseThrow(() -> new NotFoundException("Cliente não encontrado com o CPF informado."));
+    return clienteDatabaseGateway.findByCpf(cpf);
   }
   
 }
