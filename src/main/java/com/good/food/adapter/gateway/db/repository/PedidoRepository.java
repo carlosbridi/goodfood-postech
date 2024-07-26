5dcc933351780ce1d87268aad9352e2aaed2b0ea
Roberto Luiz Debarba <roberto.debarba@senior.com.br>
2024-07-26 20:41:51 -0300

    Correção PedidoRepository

--- src/main/java/com/good/food/adapter/gateway/db/repository/PedidoRepository.java
+++ src/main/java/com/good/food/adapter/gateway/db/repository/PedidoRepository.java
@@ -12,12 +12,12 @@ public interface PedidoRepository extends JpaRepository<PedidoEntity, UUID> {
 
     @Query("""
             SELECT p FROM PedidoEntity p 
-            WHERE p.status != com.good.food.adapter.gateway.db.repository.entity.EStatusPedido.FINALIZADO 
+            WHERE p.status != com.good.food.domain.entity.EStatusPedido.FINALIZADO 
             ORDER BY 
             CASE p.status 
-            WHEN com.good.food.adapter.gateway.db.repository.entity.EStatusPedido.PRONTO THEN 1 
-            WHEN com.good.food.adapter.gateway.db.repository.entity.EStatusPedido.EM_PREPARACAO THEN 2 
-            WHEN com.good.food.adapter.gateway.db.repository.entity.EStatusPedido.RECEBIDO THEN 3 
+            WHEN com.good.food.domain.entity.EStatusPedido.PRONTO THEN 1 
+            WHEN com.good.food.domain.entity.EStatusPedido.EM_PREPARACAO THEN 2 
+            WHEN com.good.food.domain.entity.EStatusPedido.RECEBIDO THEN 3 
             ELSE 4 END, p.dataCriacao ASC
             """)
     List<PedidoEntity> findAllByStatusNotFinalizadoOrderByStatusAndDate();
