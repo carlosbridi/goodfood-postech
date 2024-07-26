9e7386b90073ce1d82a7ec9a5f24588324375fa2
Roberto Luiz Debarba <roberto.debarba@senior.com.br>
2024-07-26 20:38:46 -0300

    Retornado teste GoodFoodServiceApplicationTests

---
+++ src/test/java/com/good/food/GoodFoodServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.good.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class GoodFoodServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
